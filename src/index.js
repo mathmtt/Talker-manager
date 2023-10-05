9e9c22d4ec78190228c43af702b8155b58c9032f
Matheus Motta <matheusmotta136@gmail.com>
2023-10-05 15:50:17 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const routesTalker = require('./routes/talker');
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+app.use('/talker', routesTalker);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
