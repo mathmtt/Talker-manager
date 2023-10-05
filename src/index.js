f359325153099a317c51b2f8970ee2218d947503
Matheus Motta <matheusmotta136@gmail.com>
2023-10-05 16:22:11 -0300

    requisito 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const crypto = require('crypto');
 const routesTalker = require('./routes/router');
 
 const app = express();
+
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
 
 app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 app.use('/talker', routesTalker);
 
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const token = generateToken();
+  return res.status(200).json({ token });
+});
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
--- src/routes/router.js
+++ src/routes/router.js
@@ -16,10 +16,10 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const talkers = await fs.readFile(path.join(__dirname, '../talker.json'), 'utf-8');
 
-  const talker = JSON.parse(talkers).find((talk) => talk.id === parseInt(id));
+  const talker = JSON.parse(talkers).find((talk) => talk.id === parseInt(id, 10));
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(200).json(talker);
 });
 
-  module.exports = router;
+module.exports = router;
