533a11e92058d1542158c027d4e97e7246490ab4
Matheus Motta <matheusmotta136@gmail.com>
2023-10-16 19:44:30 -0300

    requisito 5 criando o endpoint de talker

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const fs = require('fs').promises;
 const crypto = require('crypto');
-const fs = require('fs/promises');
-const path = require('path');
-const routesTalker = require('./routes/router');
-const validateData = require('./middlewares/validateData');
 
 const app = express();
 
+const path = require('path');
+
+app.use(express.json());
+const HTTP_OK_STATUS = 200;
 const BAD_REQUEST_STATUS = 400;
 const UNAUTHORIZED_STATUS = 401;
+const NOT_FOUND_STATUS = 404;
+const PORT = process.env.PORT || '3001';
+
+function validatePassword(req, res, next) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado' });
+  }
+  if (token.length !== 16) {
+    return res.status(UNAUTHORIZED_STATUS).json({ message: 'Token inválido' });
+  }
+  next();
+}
 
 function validateName(req, res, next) {
   const { name } = req.body;
@@ -22,16 +36,6 @@ function validateName(req, res, next) {
   next();
 }
 
-function validatePassword(req, res, next) {
-  const token = req.headers.authorization;
-  if (!token) {
-    return res.status(UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado' });
-  }
-  if (token.length !== 16) {
-    return res.status(UNAUTHORIZED_STATUS).json({ message: 'Token inválido' });
-  }
-  next();
-}
 function validateAge(req, res, next) {
   const { age } = req.body;
   if (!age) {
@@ -44,7 +48,28 @@ function validateAge(req, res, next) {
   next();
 }
 
-function validateWatched(req, res, next) {
+function validateLogin(req, res, next) {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+function TalkWatchedValidate(req, res, next) {
   const { talk } = req.body;
   if (!talk) {
     return res.status(BAD_REQUEST_STATUS).json({ message: 'O campo "talk" é obrigatório' });
@@ -55,17 +80,17 @@ function validateWatched(req, res, next) {
   next();
 }
 
-function validateRegex(req, res, next) {
+function TalkRegexValidate(req, res, next) {
   const { talk } = req.body;
-  const data = /^\d{2}\/\d{2}\/\d{4}$/;
-  if (!data.test(talk.watchedAt)) {
+  const tagRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (!tagRegex.test(talk.watchedAt)) {
     return res.status(BAD_REQUEST_STATUS)
       .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
   next();
 }
 
-function validateRate(req, res, next) {
+function talkRateValidate(req, res, next) {
   const { rate } = req.body.talk;
   if (!rate && rate !== 0) {
     return res.status(BAD_REQUEST_STATUS).json({ message: 'O campo "rate" é obrigatório' });
@@ -73,7 +98,7 @@ function validateRate(req, res, next) {
   next();
 }
 
-function validateNumber(req, res, next) {
+function talkNumberValidate(req, res, next) {
   const { rate } = req.body.talk;
   if (typeof rate !== 'number' || rate < 1 || rate > 5 || !Number.isInteger(rate)) {
     return res.status(BAD_REQUEST_STATUS)
@@ -82,33 +107,49 @@ function validateNumber(req, res, next) {
   next();
 }
 
-function generateToken() {
-  return crypto.randomBytes(8).toString('hex');
-}
+// (nao remover)
 
-app.use(express.json());
-const HTTP_OK_STATUS = 200;
-const PORT = process.env.PORT || '3001';
-app.use('/talker', routesTalker);
-
-app.post('/login', validateData, async (req, res) => {
-  const token = generateToken();
-  return res.status(200).json({ token });
+app.get('/', (req, res) => {
+  res.status(HTTP_OK_STATUS).send();
 });
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+app.get('/talker', async (req, res) => {
+  try {
+    const talkers = await fs.readFile(path.join(__dirname, './talker.json'), 'utf-8');
+    res.status(HTTP_OK_STATUS).json(JSON.parse(talkers)); 
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo: ${error.message}`);
+    res.status(HTTP_OK_STATUS).json([]);
+  }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const talkers = await fs.readFile(path.join(__dirname, './talker.json'), 'utf-8');
+    const talker = JSON.parse(talkers).find((talk) => talk.id === id);
+    if (!talker) {
+      return res.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    res.status(HTTP_OK_STATUS).json(talker);
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo: ${error.message}`);
+    res.status(HTTP_OK_STATUS).json([]);
+  }
+});
+app.post('/login', validateLogin, (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(HTTP_OK_STATUS).json({ token });
 });
 
 app.post('/talker',
-  validateName,
-  validatePassword,
-  validateAge,
-  validateWatched,
-  validateRegex,
-  validateRate,
-  validateNumber, async (req, res) => {
+validateAge,
+validateName,
+validatePassword,
+TalkRegexValidate,
+TalkWatchedValidate,
+  talkRateValidate,
+  talkNumberValidate, async (req, res) => {
     try {
       const { name, age, talk } = req.body;
       const talkers = await fs.readFile(path.join(__dirname, './talker.json'), 'utf-8');
@@ -123,8 +164,8 @@ app.post('/talker',
       parsedTalkers.push(newTalker);
       await fs.writeFile(path.join(__dirname, './talker.json'), JSON.stringify(parsedTalkers));
       res.status(201).json(newTalker);
-    } catch (err) {
-      console.error(`Erro ao salvar o arquivo: ${err.message}`);
+    } catch (error) {
+      console.error(`Erro ao salvar o arquivo: ${error.message}`);
       res.status(500).json({ message: 'Erro interno do servidor' });
     }
   });
--- src/middlewares/validateData.js
+++ src/middlewares/validateData.js
@@ -1,18 +1,22 @@
-const validateData = (req, res, next) => {
+function validateData(req, res, next) {
   const { email, password } = req.body;
-  const regexEmail = /\S+@\S+\.\S+/;
-  const regexPassword = /^[0-9]{6,}$/;
-
+  const BAD_REQUEST_STATUS = 400;
   if (!email) {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  } if (!regexEmail.test(email)) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   } if (!password) {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  } if (!regexPassword.test(password)) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(BAD_REQUEST_STATUS)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
-};
-
+}
+  
 module.exports = validateData;
